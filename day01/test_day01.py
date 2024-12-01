799a99196fcd0d19a800884c66fda327afc9292a
Chris Vogel <cjvogel1972@gmail.com>
2024-12-01 11:41:43 -0600

    Update gitignore and README

--- day01/test_day01.py
+++ day01/test_day01.py
@@ -29,6 +29,28 @@ class Test(unittest.TestCase):
 
         self.assertEqual(31, day.solve_part2(lines))
 
+    def test_parse_lines(self):
+        lines = [
+            "3   4",
+            "4   3",
+            "2   5",
+            "1   3",
+            "3   9",
+            "3   3"
+        ]
+
+        left, right = day.parse_lines(lines)
+        self.assertEqual(left, [3, 4, 2, 1, 3, 3])
+        self.assertEqual(right, [4, 3, 5, 3, 9, 3])
+
+    def test_count_entries(self):
+        entry_count = day.count_entries([4, 3, 5, 3, 9, 3])
+        self.assertEqual(len(entry_count), 4)
+        self.assertEqual(entry_count[3], 3)
+        self.assertEqual(entry_count[4], 1)
+        self.assertEqual(entry_count[5], 1)
+        self.assertEqual(entry_count[9], 1)
+
 
 if __name__ == '__main__':
     unittest.main()
