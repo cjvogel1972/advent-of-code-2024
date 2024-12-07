2d4f979d6914fd87e052d26e5ec82845bf825091
Chris Vogel <cjvogel1972@gmail.com>
2024-12-06 22:24:36 -0600

    Code cleanup for Day 6
    Consolidated code. Got rid of the brute force checks of the whole
    lab map and just used the positions that the guard could go to for
    a good walk through the lab. Way faster! When I was talking to
    Scott about part 2, this was my original thought on how to solve
    it, but I went for the brute force at first.

--- day06/day06.py
+++ day06/day06.py
@@ -6,16 +6,48 @@ from util.tuple import tuple_add
 
 
 def solve_part1(lines: list[str]) -> int:
+    lab, guard, direction_index = parse_lab_map(lines)
+
+    positions = determine_path(lab, guard, direction_index)
+
+    return len(positions)
+
+
+def solve_part2(lines: list[str]) -> int:
+    initial_lab, initial_guard, initial_direction_index = parse_lab_map(lines)
+
+    positions = determine_path(initial_lab, initial_guard, initial_direction_index)
+
+    obstacle_count = 0
+    for position in positions:
+            guard = initial_guard[:]
+            direction_index = initial_direction_index
+            lab = deepcopy(initial_lab)
+            lab[position[0]][position[1]] = '#'
+            if has_loop(lab, guard, direction_index):
+                obstacle_count += 1
+
+    return obstacle_count
+
+
+def parse_lab_map(lines: list[str]) -> (list[list[str]], tuple[int, int], int):
     lab = [[c for c in line] for line in lines]
+
     guard = (0, 0)
     direction_index = 0
+
     for y in range(len(lab)):
         for x in range(len(lab[0])):
             if lab[y][x] == '^':
                 guard = (y, x)
                 lab[y][x] = '.'
 
+    return lab, guard, direction_index
+
+
+def determine_path(lab: list[list[str]], guard: tuple[int, int], direction_index: int) -> dict[tuple[int, int], bool]:
     positions = {guard: True}
+
     while True:
         new_location = tuple_add(guard, cardinal_directions[direction_index])
         if not good_square(lab, *new_location):
@@ -24,38 +56,12 @@ def solve_part1(lines: list[str]) -> int:
             positions[new_location] = True
             guard = new_location
         else:
-            direction_index += 1
-            if direction_index >= len(cardinal_directions):
-                direction_index = 0
+            direction_index = turn(direction_index)
 
-    return len(positions)
+    return positions
 
 
-def solve_part2(lines: list[str]) -> int:
-    initial_lab = [[c for c in line] for line in lines]
-    initial_guard = (0, 0)
-    initial_direction_index = 0
-    for y in range(len(initial_lab)):
-        for x in range(len(initial_lab[0])):
-            if initial_lab[y][x] == '^':
-                initial_guard = (y, x)
-                initial_lab[y][x] = '.'
-
-    obstacle_count = 0
-    for y in range(len(initial_lab)):
-        for x in range(len(initial_lab[0])):
-            guard = initial_guard[:]
-            direction_index = initial_direction_index
-            lab = deepcopy(initial_lab)
-            if lab[y][x] == '.':
-                lab[y][x] = '#'
-                if has_loop(lab, guard, direction_index):
-                    obstacle_count += 1
-
-    return obstacle_count
-
-
-def has_loop(lab, guard, direction_index) -> bool:
+def has_loop(lab: list[list[str]], guard: tuple[int, int], direction_index: int) -> bool:
     loop = False
     positions = {(guard, direction_index): True}
     while True:
@@ -69,10 +75,17 @@ def has_loop(lab, guard, direction_index) -> bool:
             positions[(new_location, direction_index)] = True
             guard = new_location
         else:
-            direction_index += 1
-            if direction_index >= len(cardinal_directions):
-                direction_index = 0
+            direction_index = turn(direction_index)
+
     return loop
+
+
+def turn(direction_index):
+    direction_index += 1
+    if direction_index >= len(cardinal_directions):
+        direction_index = 0
+
+    return direction_index
 
 
 if __name__ == '__main__':
