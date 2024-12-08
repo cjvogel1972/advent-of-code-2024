183e2bceb4dce73fb17a292272e5891cbbb86043
Chris Vogel <cjvogel1972@gmail.com>
2024-12-07 19:37:18 -0600

    Code cleanup for Day 5
    Realized that if I just use the sorting of the pages following the
    rules, like in part 2, and compare to the current list, I can more
    easily figure out if the pages are in the correct order. Got the
    idea from someone's solution for day 2.

--- day05/day05.py
+++ day05/day05.py
@@ -9,7 +9,7 @@ def solve_part1(lines: list[str]) -> int:
     rules, updates = parse_input(lines)
     for line in updates:
         pages = list(map(int, line.split(",")))
-        if is_update_correct(pages, rules):
+        if pages == sort_pages(pages, rules):
             total += pages[len(pages) // 2]
 
     return total
@@ -21,8 +21,8 @@ def solve_part2(lines: list[str]) -> int:
     rules, updates = parse_input(lines)
     for line in updates:
         pages = list(map(int, line.split(",")))
-        if not is_update_correct(pages, rules):
-            fixed = reorder_pages(pages, rules)
+        fixed = sort_pages(pages, rules)
+        if pages != fixed:
             total += fixed[len(fixed) // 2]
 
     return total
@@ -44,15 +44,7 @@ def parse_input(lines: list[str]) -> tuple[dict[int, list[int]], list[str]]:
     return rules, updates
 
 
-def is_update_correct(pages, rules) -> bool:
-    for i in range(len(pages)):
-        rule = rules.get(pages[i], [])
-        if not all(page in rule for page in pages[i + 1:]):
-            return False
-    return True
-
-
-def reorder_pages(pages: list[int], rules: dict[int, list[int]]) -> list[int]:
+def sort_pages(pages: list[int], rules: dict[int, list[int]]) -> list[int]:
     return sorted(pages, key=cmp_to_key(lambda page1, page2: -1 if page2 in rules.get(page1, []) else 1))
 
 
