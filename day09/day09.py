b03ffc51a9284792a056c81da7f6cd6b715878e1
Chris Vogel <cjvogel1972@gmail.com>
2024-12-09 16:56:32 -0600

    Code cleanup for Day 9
    Pulled some common code out into functions. Simplified the logic
    in part 2. Changed how the disk blocks were stored from a string
    to an int (removing a bunch of changing from string to int (got the
    idea from HyperNeutrino on YouTube). Changed the logic for finding
    the next free block from just walking the disk to using the file
    sizes to move forward. This greatly increases the speed.

--- day09/day09.py
+++ day09/day09.py
@@ -2,108 +2,102 @@ from util.file import readfile
 
 
 def solve_part1(lines: list[str]) -> int:
-    disk_map = [c for c in lines[0]]
+    disk, file_sizes = parse_disk_map(lines[0])
 
-    file_id = 0
-    disk = []
-    file = True
-    for c in disk_map:
-        block = str(file_id) if file else "."
-        for i in range(int(c)):
-            disk.append(block)
-        if file:
-            file_id += 1
-        file = not file
-
-    next_free_block = find_next_free_block(disk, 0)
+    next_free_block = find_next_free_block(disk, file_sizes, 0)
     last_used_block = find_last_used_block(disk, len(disk) - 1)
 
     while next_free_block < last_used_block:
         disk[next_free_block] = disk[last_used_block]
-        disk[last_used_block] = '.'
-        next_free_block = find_next_free_block(disk, next_free_block + 1)
+        disk[last_used_block] = -1
+        next_free_block = find_next_free_block(disk, file_sizes, next_free_block + 1)
         last_used_block = find_last_used_block(disk, last_used_block - 1)
 
     return compute_checksum(disk)
 
 
 def solve_part2(lines: list[str]) -> int:
-    disk_map = [c for c in lines[0]]
-
-    file_id = 0
-    disk = []
-    file = True
-    for c in disk_map:
-        block = str(file_id) if file else "."
-        for i in range(int(c)):
-            disk.append(block)
-        if file:
-            file_id += 1
-        file = not file
+    disk, file_sizes = parse_disk_map(lines[0])
 
     moved = set()
     last_used_block = find_last_used_block(disk, len(disk) - 1)
 
     while True:
-        next_free_block = find_next_free_block(disk, 0)
+        next_free_block = find_next_free_block(disk, file_sizes, 0)
         if next_free_block > last_used_block:
             break
-        changed = False
-        end_file_size = 0
-        while last_used_block - end_file_size >= 0 and disk[last_used_block - end_file_size] == disk[last_used_block]:
-            end_file_size += 1
+
+        end_file_size = file_sizes[disk[last_used_block]]
 
         if disk[last_used_block] not in moved:
-            while not changed and next_free_block < last_used_block:
+            while next_free_block < last_used_block:
                 empty_block_size = 0
                 while disk[next_free_block + empty_block_size] == disk[next_free_block]:
                     empty_block_size += 1
 
                 if end_file_size <= empty_block_size:
                     moved.add(disk[last_used_block])
-                    for _ in range(end_file_size):
-                        disk[next_free_block] = disk[last_used_block]
-                        disk[last_used_block] = '.'
-                        next_free_block += 1
-                        last_used_block -= 1
-                    changed = True
+                    move_file(disk, end_file_size, last_used_block, next_free_block)
+                    break
                 else:
-                    next_free_block = find_next_free_block(disk, next_free_block + empty_block_size)
-        if not changed:
-            last_used_block = find_last_used_block(disk, last_used_block - end_file_size)
-        else:
-            last_used_block = find_last_used_block(disk, last_used_block)
-        if last_used_block == 0:
-            break
+                    next_free_block = find_next_free_block(disk, file_sizes, next_free_block + empty_block_size)
+
+        last_used_block = find_last_used_block(disk, last_used_block - end_file_size)
 
     return compute_checksum(disk)
 
 
-def find_next_free_block(disk: list[str], ptr: int) -> int:
+def parse_disk_map(disk_map: str) -> (list[int], dict[int, int]):
+    file_id = 0
+    disk = []
+    file_sizes = {}
+    file = True
+
+    for c in disk_map:
+        block = file_id if file else -1
+        for i in range(int(c)):
+            disk.append(block)
+        if file:
+            file_sizes[file_id] = int(c)
+            file_id += 1
+        file = not file
+
+    return disk, file_sizes
+
+
+def find_next_free_block(disk: list[int], files: dict[int, int], ptr: int) -> int:
     while True:
-        if disk[ptr] == ".":
+        if disk[ptr] == -1:
             break
-        ptr += 1
+        ptr += files[disk[ptr]]
 
     return ptr
 
 
-def find_last_used_block(disk: list[str], ptr: int) -> int:
+def find_last_used_block(disk: list[int], ptr: int) -> int:
     while True:
-        if disk[ptr] != ".":
+        if disk[ptr] != -1:
             break
         ptr -= 1
 
     return ptr
 
 
-def compute_checksum(disk: list[str]) -> int:
+def move_file(disk: list[int], file_size, move_from, move_to):
+    for _ in range(file_size):
+        disk[move_to] = disk[move_from]
+        disk[move_from] = -1
+        move_to += 1
+        move_from -= 1
+
+
+def compute_checksum(disk: list[int]) -> int:
     total = 0
 
     for i in range(len(disk)):
-        if disk[i] == ".":
+        if disk[i] == -1:
             continue
-        total += i * int(disk[i])
+        total += i * disk[i]
 
     return total
 
