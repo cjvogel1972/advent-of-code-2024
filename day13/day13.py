40142aaf16f280212a4ec76df6efa3d234d98d8e
Chris Vogel <cjvogel1972@gmail.com>
2024-12-19 10:20:27 -0600

    Code cleanup for Day 13
    Moved some duplicate logic to new functions.

--- day13/day13.py
+++ day13/day13.py
@@ -7,19 +7,10 @@ from util.file import readfile
 def solve_part1(lines: list[str]) -> int:
     total = 0
 
-    button_regex = re.compile("X\+(\d+), Y\+(\d+)")
-    prize_regex = re.compile("X=(\d+), Y=(\d+)")
     for i in range(0, len(lines), 4):
-        a_moves = tuple(int(x) for x in button_regex.search(lines[i]).groups())
-        b_moves = tuple(int(x) for x in button_regex.search(lines[i + 1]).groups())
-        prize = tuple(int(x) for x in prize_regex.search(lines[i + 2]).groups())
+        ax, ay, bx, by, px, py = parse_machine_behavior(lines[i:i + 4])
 
-        b_solve = ((prize[0] * a_moves[1]) - (prize[1] * a_moves[0])) / (
-                (b_moves[0] * a_moves[1]) - (a_moves[0] * b_moves[1]))
-        a_solve = (prize[0] - (b_solve * b_moves[0])) / a_moves[0]
-
-        if b_solve == math.floor(b_solve) and a_solve == math.floor(a_solve):
-            total += compute_cost(int(a_solve), int(b_solve))
+        total += cost_per_machine(ax, ay, bx, by, px, py)
 
     return total
 
@@ -27,21 +18,34 @@ def solve_part1(lines: list[str]) -> int:
 def solve_part2(lines: list[str]) -> int:
     total = 0
 
-    button_regex = re.compile("X\+(\d+), Y\+(\d+)")
-    prize_regex = re.compile("X=(\d+), Y=(\d+)")
     for i in range(0, len(lines), 4):
-        a_moves = tuple(int(x) for x in button_regex.search(lines[i]).groups())
-        b_moves = tuple(int(x) for x in button_regex.search(lines[i + 1]).groups())
-        prize = tuple(int(x) + 10000000000000 for x in prize_regex.search(lines[i + 2]).groups())
+        ax, ay, bx, by, px, py = parse_machine_behavior(lines[i:i + 4])
+        px += 10000000000000
+        py += 10000000000000
 
-        b_solve = ((prize[0] * a_moves[1]) - (prize[1] * a_moves[0])) / (
-                (b_moves[0] * a_moves[1]) - (a_moves[0] * b_moves[1]))
-        a_solve = (prize[0] - (b_solve * b_moves[0])) / a_moves[0]
-
-        if b_solve == math.floor(b_solve) and a_solve == math.floor(a_solve):
-            total += compute_cost(int(a_solve), int(b_solve))
+        total += cost_per_machine(ax, ay, bx, by, px, py)
 
     return total
+
+def cost_per_machine(ax, ay, bx, by, px, py:int) -> int:
+    b_solve = ((px * ay) - (py * ax)) / ((bx * ay) - (ax * by))
+    a_solve = (px - (b_solve * bx)) / ax
+
+    if b_solve == math.floor(b_solve) and a_solve == math.floor(a_solve):
+        return compute_cost(int(a_solve), int(b_solve))
+
+    return 0
+
+
+def parse_machine_behavior(lines: list[str]) -> tuple[int, int, int, int, int, int]:
+    button_regex = re.compile("X\+(\d+), Y\+(\d+)")
+    prize_regex = re.compile("X=(\d+), Y=(\d+)")
+
+    ax, ay = tuple(int(x) for x in button_regex.search(lines[0]).groups())
+    bx, by = tuple(int(x) for x in button_regex.search(lines[1]).groups())
+    px, py = tuple(int(x) for x in prize_regex.search(lines[2]).groups())
+
+    return ax, ay, bx, by, px, py
 
 
 def compute_cost(a: int, b: int) -> int:
